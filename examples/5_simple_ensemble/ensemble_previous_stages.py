1de8f17f665ec7adb58ce9f65ad7cc00f5a72618
Dreamlone <mik_sar@mail.ru>
2023-12-05 09:43:17 +0200

    improved ensemble

--- examples/5_simple_ensemble/ensemble_previous_stages.py
+++ examples/5_simple_ensemble/ensemble_previous_stages.py
@@ -20,8 +20,7 @@ warnings.filterwarnings('ignore')
 
 def ensemble_from_files(path: str):
     """ Collect predictions from the files and collect information about them into one prediction """
-    files_to_ensemble = ['../2_repeating/results/advanced_repeating_07_11_2023.csv',
-                         '../3_streamflow/results/usgs_streamflow_27_11_2023.csv',
+    files_to_ensemble = ['../3_streamflow/results/usgs_streamflow_27_11_2023.csv',
                          '../4_snotel/results/snotel_03_12_2023.csv',
                          '../4_snotel/results/period_30_snotel_04_12_2023.csv',
                          '../4_snotel/results/period_45_all_stations_snotel_04_12_2023.csv']
@@ -44,8 +43,8 @@ def ensemble_from_files(path: str):
             predicted_values.append(current_record.volume_50)
         predicted_values = np.array(predicted_values)
 
-        mean_value = np.mean(np.array(predicted_values))
-        adjust_ratio = 0.15
+        mean_value = np.median(np.array(predicted_values))
+        adjust_ratio = 0.23
         dataset = pd.DataFrame({'site_id': [first_submit.iloc[row_id].site_id],
                                 'issue_date': [first_submit.iloc[row_id].issue_date],
                                 'volume_10': [np.percentile(predicted_values, 10) - (mean_value * adjust_ratio)],
