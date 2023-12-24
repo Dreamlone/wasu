abd2714c13778df99aff8e66338c73ca2debeb3c
Dreamlone <mik_sar@mail.ru>
2023-12-24 14:10:22 +0200

    add basic plots

--- examples/1_basic/stream_flow_visualization.py
+++ examples/1_basic/stream_flow_visualization.py
@@ -6,7 +6,7 @@ warnings.filterwarnings('ignore')
 
 def visualize_time_series_data_with_streamflow():
     """ Streamflow visualization comparing with train data """
-    TimeSeriesPlot().usgs_streamflow(path_to_folder='../data/usgs_streamflow')
+    TimeSeriesPlot().usgs_streamflow(path_to_folder='../../data/usgs_streamflow')
 
 
 if __name__ == '__main__':
