d57956eb1fcbf242c9cb09b44de56a988462d2f5
Dreamlone <mik_sar@mail.ru>
2024-01-26 18:57:13 +0200

    added plot

--- report/generate_plot_with_metrics.py
+++ report/generate_plot_with_metrics.py
@@ -20,15 +20,20 @@ def compare_approaches():
     df = pd.DataFrame({'Mean average quantile loss': [367.66, 275.82, 193.70, 151.71, 167.40, 165.20, 137.92, 137.65, 132.90, 120.78],
                        'Method': ['Simple repeating', 'Advanced repeating',
                                   'Streamflow-based', 'SNOTEL', 'Ensemble',
-                                  'Ensemble (smoothed)', 'SNODAS', 'Complex ver 1',
+                                  'Ensemble (smoothed)', 'SNODAS', 'Complex ver 1 -\nModel on Hindcast Stage',
                                   'Complex ver 2',
-                                  'Complex (optimized)']})
+                                  'Complex (optimized) -\nModel on Forecast Stage']})
+    df_report = pd.DataFrame({'Mean average quantile loss': [137.65, 120.78],
+                              'Method': ['Complex ver 1 -\nModel on Hindcast Stage',
+                                         'Complex (optimized) -\nModel on Forecast Stage']})
 
     fig_size = (8.0, 3.0)
     fig, ax = plt.subplots(figsize=fig_size)
     plt.grid()
     plt.scatter(df['Method'], df['Mean average quantile loss'],
                 alpha=1, s=100, c='blue', edgecolor='black')
+    plt.scatter(df_report['Method'], df_report['Mean average quantile loss'],
+                alpha=0.5, s=250, c='#FFB2B2', edgecolor='red')
     plt.xlabel('Model')
     plt.ylabel('Mean average quantile loss')
     plt.xticks(rotation=75)
